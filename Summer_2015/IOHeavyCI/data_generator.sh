6bb26612a96fcbc0d91f7553331848d6c2022fa7
bth5032 <bth5032@gmail.com>
2015-06-26 14:28:39 -0700

    minor edit on text output

--- Summer_2015/IOHeavyCI/data_generator.sh
+++ Summer_2015/IOHeavyCI/data_generator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo ConcurrentReads:ReadTime:BufferSize
+echo ConcurrentReads:Velocity:BufferSize
 for k in 1 3 6 10; do
 	for j in 1024 2048 4096 8192 16384 32768 65536 131072; do
 		/nfs-7/t2tas/drop_cache
