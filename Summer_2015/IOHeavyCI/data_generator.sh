bbe505b49874861d1dd53582abf426a053a8e6e3
bth5032 <bth5032@gmail.com>
2015-06-29 11:42:18 -0700

    Made plots and archieved run2 for IOHeavyCI. Run 2 has concurrent reads and measures the velocity of the read in MBps. Setup for Run 3, including renaming the trimmed_infiles which are all 2GB.

--- Summer_2015/IOHeavyCI/data_generator.sh
+++ Summer_2015/IOHeavyCI/data_generator.sh
@@ -5,8 +5,8 @@ for k in 1 3 6 10; do
 	for j in 1024 2048 4096 8192 16384 32768 65536 131072; do
 		/nfs-7/t2tas/drop_cache
 		for i in `seq 1 $((k-1))`; do
-			./reader -f /data/tmp/IOHeavyCI/infile_$i -b $j -a $k &   
+			./reader -f /data/tmp/IOHeavyCI/trimmed_infile_$i -b $j -a $k &   
 		done
-			./reader -f /data/tmp/IOHeavyCI/infile_$k -b $j -a $k
+			./reader -f /data/tmp/IOHeavyCI/trimmed_infile_$k -b $j -a $k
 	done
 done
--- Summer_2015/IOHeavyCI/make_histograms.c
+++ Summer_2015/IOHeavyCI/make_histograms.c
@@ -17,43 +17,50 @@
 
 const short MaxNN = 6;
 */
-void make_histograms()
+void make_histograms(TString name)
 {
+	//TApplication program = new TInt 
 	//pull in data
 	TTree *t;
 	t = new TTree(); 
-	t->ReadFile("run2_output");
+	t->ReadFile("run2.output");
+	//TString name = "run2.png";
 		
 	//Initialize Canvas
 	TCanvas *c = new TCanvas("c1", "Read Velocity on Local Disk for CMS3 Files", 1920, 1080);
 	c->Divide(2,2);
 	
-	//In First Slot have Time vs. Buffer for 1 Concurrent Read
+	//In first slot have Time vs. Buffer for 1 Concurrent Read
 	c->cd(1);
 	t->SetMarkerStyle(3);
 	t->SetMarkerColor(1);
-	t->Draw("Velocity:BufferSize", "ConcurrentReads==1");
+	t->Draw("VelocityMBps:BufferSize", "ConcurrentReads==1");
 	
 	
-	/*//In Second Slot have Time vs. Buffer for 3 Concurrent Reads
+	//In second slot have Time vs. Buffer for 3 Concurrent Reads
 	c->cd(2);
 	t->SetMarkerStyle(3);
 	t->SetMarkerColor(2);
-	t->Draw("Velocity:BufferSize", "ConcurrentReads==3");
+	t->Draw("VelocityMBps:BufferSize", "ConcurrentReads==3");
 	
-	//In Second Slot have Time vs. Buffer for 3 Concurrent Reads
+	//In third slot have Time vs. Buffer for 3 Concurrent Reads
 	c->cd(3);
 	t->SetMarkerStyle(4);
 	t->SetMarkerColor(1);
-	t->Draw("Velocity:BufferSize", "ConcurrentReads==6");
+	t->Draw("VelocityMBps:BufferSize", "ConcurrentReads==6");
 	
-	//In Second Slot have Time vs. Buffer for 3 Concurrent Reads
+	//In fourth slot have Time vs. Buffer for 10 Concurrent Reads
 	c->cd(4);
 	t->SetMarkerStyle(4);
 	t->SetMarkerColor(2);
-	t->Draw("Velocity:BufferSize", "ConcurrentReads==10");
-	*/
+	t->Draw("VelocityMBps:BufferSize", "ConcurrentReads==10");
+	
 	
 	c->cd(0);
-	gPad->SaveAs("xxx.png");
+	
+	gPad->SaveAs(name);
 }
+
+/*float calculateVelocities(TH2F hist){
+	
+}*/
