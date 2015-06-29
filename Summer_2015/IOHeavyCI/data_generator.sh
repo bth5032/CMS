b36ddb31c68c43f3138d234fb4b5648a6fa6351f
bth5032 <bth5032@gmail.com>
2015-06-29 12:05:49 -0700

    fixed code in data_generator. The hacked fix for making sure all background reads exited before new reads were called has been upgraded by using a wait call after the main loop.

--- Summer_2015/IOHeavyCI/data_generator.sh
+++ Summer_2015/IOHeavyCI/data_generator.sh
@@ -4,9 +4,9 @@ echo ConcurrentReads:VelocityMBps:BufferSize
 for k in 1 3 6 10; do
 	for j in 1024 2048 4096 8192 16384 32768 65536 131072; do
 		/nfs-7/t2tas/drop_cache
-		for i in `seq 1 $((k-1))`; do
+		for i in `seq 1 $k`; do
 			./reader -f /data/tmp/IOHeavyCI/trimmed_infile_$i -b $j -a $k &   
 		done
-			./reader -f /data/tmp/IOHeavyCI/trimmed_infile_$k -b $j -a $k
+		wait
 	done
 done
