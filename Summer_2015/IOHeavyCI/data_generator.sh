057e9471de36a572a66be1f58253d7817b68584d
bth5032 <bth5032@gmail.com>
2015-07-07 14:39:59 -0700

    Added Run 9, changed data_generator to run on cabinet

--- Summer_2015/IOHeavyCI/data_generator.sh
+++ Summer_2015/IOHeavyCI/data_generator.sh
@@ -3,9 +3,9 @@
 echo ConcurrentReads:VelocityMBps:BufferSize
 for k in 1 3 6 10; do
 	for j in 1024 2048 4096 8192 16384 32768 65536 131072; do
-		#/nfs-7/t2tas/drop_cache
+		/nfs-7/t2tas/drop_cache
 		for i in `seq 1 $k`; do
-			./reader -f /hadoop/cms/store/user/matevz/IOHeavyCI/trimmed_infile_$i -b $j -a $k &   
+			./reader -f /data$i/xrootd/trimmed_infile_$i -b $j -a $k &   
 		done
 		wait
 	done
