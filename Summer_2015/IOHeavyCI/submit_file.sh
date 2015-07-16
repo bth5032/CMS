5bffed5ea0e13994eccc76ef67312fc74388f605
bth5032 <bth5032@gmail.com>
2015-07-16 12:11:13 -0700

    Fixed typo in submit_file.sh

--- Summer_2015/IOHeavyCI/submit_file.sh
+++ Summer_2015/IOHeavyCI/submit_file.sh
@@ -3,9 +3,9 @@ then
 	echo ConcurrentReads:VelocityMBps:BufferSize
 	for k in 1 3 6 10; do
 		for j in 1024 2048 4096 8192 16384 32768 65536 131072; do
-			/nfs-7/t2tas/drop_cache
+			echo "/nfs-7/t2tas/drop_cache"
 			for i in `seq 3 $(($k+2))`; do
-				./nfs-7/t2tas/reader -f /data$i/xrootd/trimmed_infile_$i -b $j -a $k &   
+				echo "./nfs-7/t2tas/reader -f /data$i/xrootd/trimmed_infile_$i -b $j -a $k &"   
 			done
 			wait
 		done
