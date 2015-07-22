f8874a0dabf52c4e81b37ad012e98d51671ac61a
bth5032 <bth5032@gmail.com>
2015-07-22 13:43:55 -0700

    Added support for temp file writes in submit_file. Now the tests will copy the output of processes in slot33 to /tmp/BobaksReadTestOutput and the output of the other processes to /tmp/BobaksIgnoredOutput

--- Summer_2015/IOHeavyCI/submit_file.sh
+++ Summer_2015/IOHeavyCI/submit_file.sh
@@ -6,14 +6,13 @@ then
 		for j in 1024 2048 4096 8192 16384 32768 65536 131072; do
 			/nfs-7/t2tas/drop_cache
 			for i in `seq 3 $(($k+2))`; do
-				/nfs-7/t2tas/reader -f /data$i/xrootd/trimmed_infile_$i -b $j -a "$k    $_CONDOR_SLOT" &   
+				/nfs-7/t2tas/reader -f /data$i/xrootd/trimmed_infile_$i -b $j -a "$k    $_CONDOR_SLOT" | tee -a /tmp/BobaksReadTestOutput &   
 			done
 			wait
 		done
 	done
-	sleep 600
 else
-	echo "--Ignored Slot--"
-	#sleep 600 
-	echo `uname -n` : $_CONDOR_SLOT
+	echo "--Ignored Slot--" | tee -a /tmp/BobaksIgnoredOutput
+	echo `uname -n` : $_CONDOR_SLOT | tee -a /tmp/BobaksIgnoredOutput
+	sleep 600
 fi
